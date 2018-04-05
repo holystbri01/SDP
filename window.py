ae837bc6ec87f7edaf3bd9bb720778729121e136
holystbri01 <briar.holyst@uconn.edu>
2018-04-05 15:52:53 +0000

    Created grid, need to fix spacing/stretch of labels

--- window.py
+++ window.py
@@ -7,32 +7,37 @@ class gridlayout(QWidget):
 	def __init__(self):
 		super(QWidget, self).__init__()
 
-		lab1 = QLabel("Label1")
-		line_edit1 = QLineEdit()
-		sub1 = QVBoxLayout()
-		sub1.addWidget(lab1)
-		sub1.addWidget(line_edit1)
-
-		lab2 = QLabel("Label2")
-		line_edit2 = QLineEdit()
-		sub2 = QVBoxLayout()
-		sub2.addWidget(lab2)
-		sub2.addWidget(line_edit2)
-
+		label1 = ImageLabel(QPixmap("current devices button.png"))
+		label2 = ImageLabel(QPixmap("add device button.png"))
+		label3 = ImageLabel(QPixmap("remove device button.png"))
+		label4 = ImageLabel(QPixmap("thermostats button.png"))
+		label5 = ImageLabel(QPixmap("security log button.png"))
+		label6 = ImageLabel(QPixmap("settings button.png"))
+		
 		button1 = ImageButton(QPixmap("devices current icon.png"))
 		button2 = ImageButton(QPixmap("add button.png"))
 		button3 = ImageButton(QPixmap("remove button.png"))
 		button4 = ImageButton(QPixmap("thermostat button.png"))
 		button5 = ImageButton(QPixmap("view log button.png"))
+		button6 = ImageButton(QPixmap("settings icon.png"))
 
 		grid_layout = QGridLayout(self)
-		grid_layout.addLayout(sub1, 0, 0, 1, 3)
-		grid_layout.addLayout(sub2, 1, 0, 1, 3)
-		grid_layout.addWidget(button1, 2, 0, 1, 1)
-		grid_layout.addWidget(button2, 2, 1, 1, 1)
-		grid_layout.addWidget(button3, 2, 2, 1, 1)
-		grid_layout.addWidget(button4, 3, 0, 1, 1)
-		grid_layout.addWidget(button5, 3, 1, 1, 1)
+	# first row of buttons
+		grid_layout.addWidget(button1, 0, 0, 1, 1, Qt.AlignCenter)
+		grid_layout.addWidget(button2, 0, 3, 2, 1, Qt.AlignCenter)
+		grid_layout.addWidget(button3, 0, 5, 2, 1, Qt.AlignCenter)
+	#first row of labels
+		grid_layout.addWidget(label1, 2, 0, 1, 1)
+		grid_layout.addWidget(label2, 2, 3, 1, 1)
+		grid_layout.addWidget(label3, 2, 5, 1, 1)
+	#second row of buttons
+		grid_layout.addWidget(button4, 4, 0, 2, 1, Qt.AlignCenter)
+		grid_layout.addWidget(button5, 4, 3, 2, 1, Qt.AlignCenter)
+		grid_layout.addWidget(button6, 4, 5, 2, 1, Qt.AlignCenter)
+	#second row of labels
+		grid_layout.addWidget(label4, 6, 0, 1, 1)
+		grid_layout.addWidget(label5, 6, 3, 1, 1)
+		grid_layout.addWidget(label6, 6, 5, 1, 1)
 
 		self.show()
 
